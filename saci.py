3b3b8cce2f00e042ca93e765d6f79a94be5c32dd
Henrique_Nardy <hnardyluz@gmail.com>
2023-11-15 17:15:42 -0300

    começo de testes com interface de apresentação

--- saci.py
+++ saci.py
@@ -1,9 +1,31 @@
 from tkinter import *
 import turtle
 import sys
+import os
+
+def start():
+
+    pasta = os.path.dirname(__file__)
+
+    op = Tk()
+    op.grid()
+    op.geometry("1200x720")
+    op.title("SACI")
+
+    imagem_blender = PhotoImage(file=pasta+"\\img1.gif")
+    foto = Label(op, image=imagem_blender)
+    foto.pack()
+    stbt = Button(op,text="INICIAR", command=op.destroy)
+    stbt.place(x=600,y=650)
+    op.mainloop()
+    
+
+
 
 
 def ligar():
+
+
     def fechar():
         win.destroy()
 
@@ -22,7 +44,7 @@ def ligar():
             turtle.bgcolor("black")
 
             my_pen.speed(velocity)
-            colors = ["red", "turquoise", "green", "pink", "blue", "yellow", "purple", "white"]
+            pencolors = ["red", "turquoise", "green", "pink", "blue", "yellow", "purple", "white"]
 
             my_pen.speed(velocity)
 
@@ -38,7 +60,7 @@ def ligar():
                 my_pen.width(espe)
                 turtle.title(f"desenhando: {100 * f / repeats :.2f}%")
 
-                my_pen.pencolor(colors[f % len(colors)])
+                my_pen.pencolor(pencolors[f % len(pencolors)])
                 my_pen.forward(lin)
                 my_pen.left(angmod)
 
@@ -54,7 +76,7 @@ def ligar():
     win = Tk()
     win.grid()
     win.geometry("400x400")
-    win.title("SACI (nome provisório)")
+    win.title("SACI)")
 
     l1 = Label(win, text="escolha o ângulo")
     l1.grid(column=5, row=0)
@@ -62,16 +84,16 @@ def ligar():
     a = Spinbox(win, from_=10, to=359)
     a.grid(column=5, row=2)
 
-    l2 = Label(win, text="digite a regra de crescimento (recomendado 0.2)")
+    l2 = Label(win, text="digite  o fator de crescimento ")
     l2.grid(column=5, row=4)
 
     b = Spinbox(win, from_=0.1, to=10.0, increment=0.1, )
     b.grid(column=5, row=6)
 
-    l3 = Label(win, text="digite o número de repetições (recomendado 500)")
+    l3 = Label(win, text="digite o número de repetições ")
     l3.grid(column=5, row=8)
 
-    c = Spinbox(win, from_=100, to=1000, increment=100)
+    c = Spinbox(win, from_=100, to=10000, increment=100)
     c.grid(column=5, row=10)
 
     l4 = Label(win, text="espessura da linha")
@@ -80,7 +102,7 @@ def ligar():
     d = Spinbox(win, from_=1, to=10, increment=1)
     d.grid(column=5, row=14)
 
-    l5 = Label(win, text="fator vertical ")
+    l5 = Label(win, text=" fator de engrossamento da linha ")
     l5.grid(column=5, row=16)
 
     e = Spinbox(win, to=1, increment=0.01)
@@ -91,10 +113,17 @@ def ligar():
     bot = Button(win, text="iniciar", command=ligar_tartaruga)
     bot.grid(column=5, row=20)
 
-    bot = Button(win, text="fechar", command=fechar)
-    bot.grid(column=6, row=20)
+   # bot = Button(win, text="fechar", command=fechar)
+   # bot.grid(column=6, row=20)
+
+    l5 = Label(win, text="seleção de cores")
+    l5.grid(column=5, row= 22)
+
+    b_preto = Button(win,)
 
     win.mainloop()
 
 
+
+#start()
 ligar()
